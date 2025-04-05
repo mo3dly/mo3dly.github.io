bdd0bf2be028892b206af276dffc058b1c7a8267
Fayez <i9fayez@proton.me>
2025-04-05 14:56:32 +0300

    Some fixes

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,16 +1,37 @@
+let selectedGrade = null;
+let selectedTerm = null;
+let choosingTerm = false;
+
 function selectGrade(card) {
     const cards = document.querySelectorAll('.grade-card');
     cards.forEach(c => c.classList.remove('selected'));
     card.classList.add('selected');
+    selectedGrade = card.dataset.grade;
 }
 
-
 function proceed() {
-    const selected = document.querySelector('.grade-card.selected');
-    if (selected) {
-        let gradeNumber = selected.dataset;  
-        window.location.href = `/grades/${gradeNumber}`;
+    if (!choosingTerm) {
+        if (selectedGrade) {
+            choosingTerm = true;
+            document.querySelector('.grade-selection').style.display = 'none';
+            document.querySelector('.term-selection').style.display = 'flex';
+            document.querySelector('h1').innerText = '📅 اختر الفصل الدراسي';
+            document.querySelector('.next-button').innerText = 'التالي →';
+        } else {
+            alert('الرجاء اختيار الصف أولاً');
+        }
     } else {
-        alert('الرجاء اختيار الفصل أولاً');
+        if (selectedTerm) {
+            window.location.href = `/grades/${selectedGrade}/term${selectedTerm}.html`;
+        } else {
+            alert('الرجاء اختيار الفصل الدراسي أولاً');
+        }
     }
+}
+
+function selectTerm(card) {
+    const cards = document.querySelectorAll('.term-card');
+    cards.forEach(c => c.classList.remove('selected'));
+    card.classList.add('selected');
+    selectedTerm = card.dataset.term;
 }
