42a33013100df58c453548ffa1b926f22d99e765
Fayez <i9Fayez@proton.me>
2026-01-29 12:14:30 +0300

    feat: add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Cairo } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const cairo = Cairo({
   subsets: ["arabic"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <GoogleAnalytics gaId="G-46KDT1HD6W" />
       </body>
     </html>
   );
