b27f721852153af371280e8731183db5606181d8
Fayez <106971733+me38ly@users.noreply.github.com>
2026-09-10 09:53:00 +0300

    Integrate Google Ads script in layout
    Added Google Ads script to the layout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { describe } from "node:test";
+import Script from "next/script";
 
 const cairo = Cairo({
   subsets: ["arabic"],
@@ -71,6 +71,12 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4968434285942225"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <GoogleAnalytics gaId="G-46KDT1HD6W" />
       </body>
     </html>
